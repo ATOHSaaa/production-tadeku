b3e67f8485a6aa21f7747d83995cc5b9cc7db90b
ATOHSaaa <atohslit1113@gmail.com>
2025-09-28 17:14:36 +0900

    設定変更

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,8 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://atohsaaa.github.io',
+  base: 'production-tadeku',
+});
+
